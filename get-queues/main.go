b5ff5996840158aac9259e42dd59eb3d2f67e567
nbari <nbari@tequila.io>
2019-02-14 10:10:21 +0100

    sync Thu Feb 14 10:10:21 CET 2019

--- get-queues/main.go
+++ get-queues/main.go
@@ -5,19 +5,38 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	rUSER := "bunny"
+	rPASS := "test"
+	rHOST := "my-rabbit"
+	rPORT := "15672" // string to make it easy dealign with the env
+
+	// read from ENV
+	if e := os.Getenv("RABBITMQ_USER"); e != "" {
+		rUSER = e
+	}
+	if e := os.Getenv("RABBITMQ_PASS"); e != "" {
+		rPASS = e
+	}
+	if e := os.Getenv("RABBITMQ_HOST"); e != "" {
+		rHOST = e
+	}
+	if e := os.Getenv("RABBITMQ_PORT"); e != "" {
+		rPORT = e
+	}
 
 	type Queue struct {
 		Name  string `json:name`
 		VHost string `json:vhost`
 	}
 
-	manager := "http://127.0.0.1:15672/api/queues/"
+	manager := fmt.Sprintf("http://%s:%s/api/queues/", rHOST, rPORT)
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", manager, nil)
-	req.SetBasicAuth("bunny", "test")
+	req.SetBasicAuth(rUSER, rPASS)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
